682b8aeab64df6612e6c03c69d42adabca8c95a0
nouran-essam <nouranessam885@gmail.com>
2025-08-30 01:40:32 +0300

    d

--- Introduction_to_Django/bookshelf/admin.py
+++ Introduction_to_Django/bookshelf/admin.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
 from .models import Book
 
-# تخصيص عرض الموديل في Admin
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')  # الأعمدة الظاهرة في القائمة
-    search_fields = ('title', 'author')                     # إضافة البحث حسب العنوان أو المؤلف
-    list_filter = ('publication_year',)                    # فلترة حسب سنة النشر
+    list_display = ('title', 'author', 'publication_year')
+    search_fields = ('title', 'author')
+    list_filter = ('publication_year',)
 
-# تسجيل الموديل مع التخصيص
 admin.site.register(Book, BookAdmin)
